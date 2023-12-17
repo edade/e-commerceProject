52b923219d5b9c096e6629b288220f7d2c1be33b
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-12-17 17:19:51 +0300

    header component düzenlendi

--- e-commerce/src/layout/Header.js
+++ e-commerce/src/layout/Header.js
@@ -1,8 +1,43 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useLocation } from "react-router-dom";
 const Header = () => {
+  const location = useLocation;
+  const isProductListPage = location.pathname === "/products";
+
   return (
     <header>
+      {isProductListPage && (
+        <div className=" bg-[#23856D] w-full font-['montserrat'] ">
+          <div className="flex justify-evenly container mx-auto p-4 h-12 items-center text-white">
+            <div className="flex items-center font-normal text-sm ">
+              <img
+                className="h-[1rem] w-[1rem] mr-1"
+                src="./img/productlistphone.png"
+                alt="Home"
+              />
+              <p>(225) 555-0118</p>
+            </div>
+            <div className="flex items-center font-normal text-sm ">
+              <img
+                className="h-[1rem] w-[1rem] mr-1"
+                src="./img/productlistemail.png"
+                alt="Home"
+              />
+              <p>michelle.rivera@example.com</p>
+            </div>
+            <div className="font-bold text-sm">
+              <p>Follow Us and get a chance to win 80% off</p>
+            </div>
+            <div className="flex justify-between items-center font-bold text-sm gap-3">
+              <h6>Follow Us :</h6>
+              <img className="h-[1em] " src="./img/PLinsta.png" alt="" />
+              <img className="h-[1em] " src="./img/PLyoutube.png" alt="" />
+              <img className="h-[1em] " src="./img/PLfb.png" alt="" />
+              <img className="h-[1em] " src="./img/PLtwitter.png" alt="" />
+            </div>
+          </div>
+        </div>
+      )}
       <div className="flex justify-between mt-2 items-center h-[4rem] font-['montserrat']">
         <Link
           to="/"
@@ -21,22 +56,34 @@ const Header = () => {
           </nav>
           <div className="flex">
             <div className="flex items-center w-[12.5rem] justify-around px-4 font-semibold text-[#23A6F0]">
-            <img className="h-[1em] " src="./img/login.png" alt="" />
-            
-              <NavLink  to="/login">Login /</NavLink>
+              <img className="h-[1em] " src="./img/login.png" alt="" />
+
+              <NavLink to="/login">Login /</NavLink>
 
               <NavLink to="/register">Register</NavLink>
             </div>
             <div className="flex w-[9.875rem] justify-between px-3 items-center text-[#23A6F0]">
-                <div className="flex items-center">
-                <img className="h-[1em] items-center" src="./img/search.png" alt="" />
-                </div>
               <div className="flex items-center">
-              <img className="h-[1em] items-center" src="./img/shop.png" alt="" />
+                <img
+                  className="h-[1em] items-center"
+                  src="./img/search.png"
+                  alt=""
+                />
+              </div>
+              <div className="flex items-center">
+                <img
+                  className="h-[1em] items-center"
+                  src="./img/shop.png"
+                  alt=""
+                />
                 <p className="ml-1">3</p>
               </div>
               <div className="flex items-center">
-              <img className="h-[1em] items-center" src="./img/like.png" alt="" />
+                <img
+                  className="h-[1em] items-center"
+                  src="./img/like.png"
+                  alt=""
+                />
                 <p className="ml-1">10</p>
               </div>
             </div>
