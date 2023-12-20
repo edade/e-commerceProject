ea2e22d762fcc64ebc54e736626b052413602f41
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-12-20 23:55:50 +0300

    video part done

---
+++ e-commerce/src/components/AboutPage/Video.js
@@ -0,0 +1,11 @@
+const Video = () => {
+    return (
+        <div className="max-w-screen-lg mx-auto py-28">
+
+        <video controls className="rounded-lg" poster="./img/AboutPage/video.png">
+          <source src="./img/AboutPage/video1.mp4" type="video/mp4" />
+        </video>
+      </div>
+    )
+}
+export default Video;
