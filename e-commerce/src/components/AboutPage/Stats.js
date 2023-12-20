0d0b4879337484b30b9a202c79b2ca6c0931c0e7
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-12-20 23:34:31 +0300

    stats done

---
+++ e-commerce/src/components/AboutPage/Stats.js
@@ -0,0 +1,11 @@
+const Stats = () => {
+    return (
+        <div className="flex flex-row justify-between items-center mx-60 text-[#252B42] font-bold text-4xl py-8 ">
+            <div className="flex flex-col">15K <p className="text-sm font-semibold text-[#737373]">Happy Customers</p></div>
+            <div className="flex flex-col">150K <p className="text-sm font-semibold text-[#737373]">Monthly Visitors</p> </div>
+            <div className="flex flex-col">15 <p className="text-sm font-semibold text-[#737373]">Countries Worldwide</p></div>
+            <div className="flex flex-col">100+ <p className="text-sm font-semibold text-[#737373]">Top Partners</p></div>
+        </div>
+    )
+}
+export default Stats;
