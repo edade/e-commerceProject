f375516794a95b1cd39181dadf07838b7ae3075f
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-12-27 16:40:54 +0300

    store part is created requirement will be implemented

--- e-commerce/src/components/SignUp.js
+++ e-commerce/src/components/SignUp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import Form from "react-bootstrap/Form";
 import { Button } from "@material-tailwind/react";
 
@@ -8,7 +8,13 @@ const SignUp = () => {
     register,
     handleSubmit,
     formState: { errors },
+    control, 
+    watch,
   } = useForm();
+
+  const password = watch("password", "");
+  const role = watch("role", "customer");
+
   const onSubmit = (data) => {
     console.log(data);
   };
@@ -38,42 +44,150 @@ const SignUp = () => {
         <Form.Group className="py-4" controlId="email">
           <Form.Control
             type="email"
-            {...register("email", { required: "Please enter your email",  validate: (val: any) => {
-                const emailRegex =
-                  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-                if (!emailRegex.test(val)) {
-                  return "Please enter a valid email address.";
-                }
+            {...register("email", {
+              required: "Please enter your email",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Try a valid email address",
               },
-             })}
+            })}
             placeholder="Enter your Email"
           />
-        {errors.email && (
-            <Form.Text className="text-danger">{errors.email.message}</Form.Text>
+          {errors.email && (
+            <Form.Text className="text-danger">
+              {errors.email.message}
+            </Form.Text>
           )}
         </Form.Group>
 
         <Form.Group controlId="password">
           <Form.Control
             type="password"
-            {...register("password", { required: " Please confirm your password",  minLength: {
+            {...register("password", {
+              required: " Please confirm your password",
+              minLength: {
                 value: 8,
                 message: "At least 8 character required",
-              }, })}
+              },
+              validate: {
+                uppercase: (value) =>
+                  /[A-Z]/.test(value) ||
+                  "Password must contain at least one uppercase letter",
+                lowercase: (value) =>
+                  /[a-z]/.test(value) ||
+                  "Password must contain at least one lowercase letter",
+                number: (value) =>
+                  /\d/.test(value) ||
+                  "Password must contain at least one number",
+                specialChar: (value) =>
+                  /[!@#$%^&*(),.?":{}|<>]/.test(value) ||
+                  "Password must contain at least one special character",
+              },
+            })}
             placeholder="Confirm password"
           />
-        {errors.password && (
-            <Form.Text className="text-danger">{errors.password.message}</Form.Text>
+          {errors.password && (
+            <Form.Text className="text-danger">
+              {errors.password.message}
+            </Form.Text>
           )}
         </Form.Group>
 
-        <Form.Group className="py-4 flex items-center gap-2 " controlId="role">
-          <Form.Label>Role</Form.Label>
-          <Form.Control as="select" placeholder="Store">
-            <option value="user">Customer</option>
-            <option value="admin">Store</option>
-          </Form.Control>
+        <Form.Group className="pt-4" controlId="confirmPassword">
+          <Form.Control
+            type="password"
+            {...register("confirmPassword", {
+              required: "Please confirm your password",
+              validate: (value) =>
+                value === password || "Passwords do not match",
+            })}
+            placeholder="Confirm password"
+          />
+          {errors.confirmPassword && (
+            <Form.Text className="text-danger">
+              {errors.confirmPassword.message}
+            </Form.Text>
+          )}
         </Form.Group>
+
+        <Form.Group className="py-4 flex items-center gap-2" controlId="role">
+          <Form.Label>Role</Form.Label>
+          <Controller
+            name="role"
+            control={control}
+            defaultValue="customer"
+            render={({ field }) => (
+              <Form.Control as="select" {...field}>
+                <option value="customer">Customer</option>
+                <option value="store">Store</option>
+              </Form.Control>
+            )}
+          />
+        </Form.Group>
+
+        {role === "store" && (
+          <>
+            <Form.Group controlId="storeName">
+              <Form.Label>Store Name</Form.Label>
+              <Controller
+                name="storeName"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <Form.Control type="text" {...field} />
+                )}
+              />
+              {errors.storeName && (
+                <Form.Text className="text-danger">{errors.storeName.message}</Form.Text>
+              )}
+            </Form.Group>
+
+            <Form.Group controlId="storePhone">
+              <Form.Label>Store Phone</Form.Label>
+              <Controller
+                name="storePhone"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <Form.Control type="text" {...field} />
+                )}
+              />
+              {errors.storePhone && (
+                <Form.Text className="text-danger">{errors.storePhone.message}</Form.Text>
+              )}
+            </Form.Group>
+
+            <Form.Group controlId="storeTaxId">
+              <Form.Label>Store Tax ID</Form.Label>
+              <Controller
+                name="storeTaxId"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <Form.Control type="text" {...field} />
+                )}
+              />
+              {errors.storeTaxId && (
+                <Form.Text className="text-danger">{errors.storeTaxId.message}</Form.Text>
+              )}
+            </Form.Group>
+
+            <Form.Group controlId="storeBankAccount">
+              <Form.Label>Store Bank Account</Form.Label>
+              <Controller
+                name="storeBankAccount"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <Form.Control type="text" {...field} />
+                )}
+              />
+              {errors.storeBankAccount && (
+                <Form.Text className="text-danger">{errors.storeBankAccount.message}</Form.Text>
+              )}
+            </Form.Group>
+          </>
+        )}
         <Button type="submit">Submit</Button>
       </Form>
     </div>
