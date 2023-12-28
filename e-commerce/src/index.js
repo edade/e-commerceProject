ed87dbe767cd97d3454b95269ca55e6805dd7ba4
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-12-28 13:18:17 +0300

    reducers created and provider added in index

--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@material-tailwind/react';
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@material-tailwind/react";
+import { Provider } from "react-redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { store } from "../src/store/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<BrowserRouter>
-<ThemeProvider>
-<App />
-</ThemeProvider>
-</BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>
 );
--- e-commerce/src/store/store.js
+++ e-commerce/src/store/store.js
@@ -1,2 +1,19 @@
-import {legacy_createStore as createStore} from "redux";
-export const store = createStore(reducer);
+import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import { GlobalReducer } from "./reducers/GlobalReducer";
+import { userReducer } from "./reducers/UserReducer";
+import { ProductReducer } from "./reducers/ProductReducer";
+import { ShoppingCardReducer } from "./reducers/ShoppingCardReducer";
+import { thunk } from "redux-thunk";
+import logger from "redux-logger";
+
+
+export const reducers = combineReducers({
+    global: GlobalReducer,
+    user:userReducer,
+    product: ProductReducer,
+    shoppingCard: ShoppingCardReducer,
+
+
+})
+
+export const store = createStore(reducers, applyMiddleware(logger, thunk));
