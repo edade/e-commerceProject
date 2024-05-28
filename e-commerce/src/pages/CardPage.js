7020f81614533244536465275004d44f50d521e3
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-05-28 23:09:32 +0300

    payment options are shown

--- e-commerce/src/pages/CardPage.js
+++ e-commerce/src/pages/CardPage.js
@@ -10,6 +10,7 @@ import AddCardForm from "../components/AddCardForm";
 import EditCardForm from "../components/EditCardForm";
 import { UpdateCard } from "../store/thunk/UpdateCard";
 import { RemoveCard } from "../store/thunk/RemoveCard";
+import { calculateInstallments } from "../components/CalculateInstallments";
 
 const CardPage = () => {
   const cartItems = useSelector((state) => state.shoppingCard.cart);
@@ -19,6 +20,8 @@ const CardPage = () => {
   const userToken = useSelector((state) => state.user.token);
   const [showCardForm, setShowCardForm] = useState(false);
   const [showEditForm, setShowEditForm] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null);
+
   const [formData, setFormData] = useState({
     card_no: "",
     expire_month: "",
@@ -101,6 +104,18 @@ const CardPage = () => {
     );
   };
 
+  const handleCardSelect = (card) => {
+    setSelectedCard(card);
+  };
+
+  const totalAmount = calculateGrandTotalPrice();
+  const defaultInstallmentOption = [
+    { installment: 0, monthlyPayment: totalAmount, totalPayment: totalAmount },
+  ];
+  const paymentOptions = selectedCard
+    ? calculateInstallments(totalAmount)
+    : defaultInstallmentOption;
+
   return (
     <div>
       <Header />
@@ -119,63 +134,89 @@ const CardPage = () => {
             Kayıtlı kart bilgilerinizi görüntüleyebilir ve yeni kart
             ekleyebilirsiniz.
           </div>
-          <div className="flex flex-col border-3 border-[#9bc8e3]-400 rounded-lg gap-3">
-            <div className="flex justify-between flex-row">
-              <div>Kayıtlı Kartlarım</div>
-            </div>
-            <div className="flex flex-row flex-wrap justify-around px-3">
-              <div className="flex flex-col border-3 my-3 h-40 w-2/4 border-[#9bc8e3]-400 rounded-lg justify-center">
-                <Button
-                  variant="outline-primary"
-                  className="h-full"
-                  onClick={handleAddCardClick}
-                >
-                  <i className="fa-solid fa-plus "></i>
-                  Yeni Kart Ekle
-                </Button>
+          <div className="flex flex-row">
+            <div className="flex flex-col border-3 border-[#9bc8e3]-400 rounded-lg w-2/3">
+              <div className="flex text-lg font-bold justify-between flex-row">
+                <div>Kayıtlı Kartlarım</div>
               </div>
-              {cards.map((card) => (
-                <div
-                  key={card.id}
-                  className="border-3 my-3 p-3 h-40 border-[#9bc8e3]-400 rounded-lg w-2/4"
-                >
-                  <p className="text-left py-2">
-                    Kart Sahibi: {card.name_on_card}
-                  </p>
-                  <div className="flex text-left flex-row justify-between">
-                    <p> Kart Numarası: {card.card_no}</p>
-                    <p>
-                      Son Kullanma Tarihi: {card.expire_month}/
-                      {card.expire_year}
+              <div className="flex flex-row flex-wrap px-1">
+                <div className="flex flex-col border-3 my-3 h-40 w-2/4 border-[#9bc8e3]-400 rounded-lg justify-center">
+                  <Button
+                    variant="outline-primary"
+                    className="h-full"
+                    onClick={handleAddCardClick}
+                  >
+                    <i className="fa-solid fa-plus "></i>
+                    Yeni Kart Ekle
+                  </Button>
+                </div>
+                {cards.map((card) => (
+                  <div
+                    key={card.id}
+                    className={`border-3 my-3 p-3 h-40 border-[#9bc8e3]-400 rounded-lg w-1/2 ${
+                      selectedCard && selectedCard.id === card.id
+                        ? "border-blue-500"
+                        : ""
+                    }`}
+                    onClick={() => handleCardSelect(card)}
+                  >
+                    <p className="text-left py-2">
+                      Kart Sahibi: {card.name_on_card}
                     </p>
-                  </div>
+                    <div className="flex text-left flex-row justify-between">
+                      <p> Kart Numarası: {card.card_no}</p>
+                      <p>
+                        Son Kullanma Tarihi: {card.expire_month}/
+                        {card.expire_year}
+                      </p>
+                    </div>
 
-                  <div className="text-end">
-                    <button
-                      className="text-gray-600 mr-3"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        handleDeleteCard(card.id);
-                      }}
-                    >
-                      Sil
-                    </button>
-                    <button
-                      className="text-right py-2 text-blue-400"
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        handleEditCardClick(card);
-                      }}
-                    >
-                      Düzenle
-                    </button>
+                    <div className="text-end">
+                      <button
+                        className="text-gray-600 mr-3"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleDeleteCard(card.id);
+                        }}
+                      >
+                        Sil
+                      </button>
+                      <button
+                        className="text-right py-2 text-blue-400"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleEditCardClick(card);
+                        }}
+                      >
+                        Düzenle
+                      </button>
+                    </div>
                   </div>
+                ))}
+              </div>
+            </div>
+
+            <div className=" flex flex-col border-3 border-[#9bc8e3]-400 rounded-lg p-3 w-1/3">
+              <h3 className="text-lg font-bold">Taksit Seçenekleri</h3>
+              <div className="grid grid-cols-3 gap-4 border-t border-gray-300 py-2">
+                <p className="font-bold">Taksit Sayısı</p>
+                <p className="font-bold">Aylık Ödeme</p>
+                <p className="font-bold">Toplam Ödeme</p>
+              </div>
+              {paymentOptions.map((option, index) => (
+                <div
+                  key={index}
+                  className="grid grid-cols-3 gap-4 border-t border-gray-300 py-2"
+                >
+                  <p>{option.installment}</p>
+                  <p>{option.monthlyPayment} TL</p>
+                  <p>{option.totalPayment} TL</p>
                 </div>
               ))}
             </div>
           </div>
         </div>
-        <div className="border-3 border-[#9bc8e3]-400  rounded-lg w-[20%] py-2 h-2/5 text-sm">
+        <div className="flex flex-col border-3 border-[#9bc8e3]-400  rounded-lg w-[20%] py-2 h-2/5 text-sm">
           <button className="text-white bg-[#23A6F0] py-3 px-4 rounded mb-3">
             Kaydet ve Devam Et
           </button>
@@ -211,12 +252,14 @@ const CardPage = () => {
         handleClose={handleCloseAddCardForm}
         handleSaveCard={handleSaveCard}
       />
-      <EditCardForm
-        show={showEditForm}
-        handleClose={handleCloseEditCardForm}
-        card={formData}
-        handleSaveCard={handleUpdateCard}
-      />
+      {selectedCard && (
+        <EditCardForm
+          show={showEditForm}
+          handleClose={handleCloseEditCardForm}
+          card={selectedCard}
+          handleSaveCard={handleUpdateCard}
+        />
+      )}
     </div>
   );
 };
