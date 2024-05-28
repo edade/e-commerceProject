9c847c48f9663a106dff6035e8221b75dfff7bfc
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-05-28 23:08:51 +0300

    calculation of installments update

--- e-commerce/src/components/CalculateInstallments.js
+++ e-commerce/src/components/CalculateInstallments.js
@@ -1,13 +1,12 @@
 export const calculateInstallments = (totalAmount) => {
   const options = [];
   for (let i = 0; i <= 6; i += 2) {
-    const monthlyPayment = i === 0 ? totalAmount : (totalAmount / i).toFixed(2);
-    const totalPayment =
-      i === 0 ? totalAmount : (monthlyPayment * i).toFixed(2);
+    const totalWithInterest = (totalAmount * (1 + 0.1 * (i / 2))).toFixed(2);
+    const monthlyPayment = (totalWithInterest / (i || 1)).toFixed(2);
     options.push({
       installment: i === 0 ? "Tek Çekim" : i,
       monthlyPayment: monthlyPayment,
-      totalPayment: totalPayment,
+      totalPayment: totalWithInterest,
     });
   }
   return options;
